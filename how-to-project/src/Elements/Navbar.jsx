4b3e9ace1bbb1f1cebadfa958159b792ba6fa78f
Muskan Gupta <1218.muskangupta@gmail.com>
2023-03-08 01:53:08 +0530

    Navbar CSS

--- how-to-project/src/Elements/Navbar.jsx
+++ how-to-project/src/Elements/Navbar.jsx
@@ -1,12 +1,48 @@
+import React, { useState } from "react";
 import Logo from "../Images/Logo.png";
 import Down_arrow from "../Images/down-arrow.png";
+import PopUp from "./PopUp.jsx";
 import LoginIcon from "./login";
-import {auth } from "../Firebase/firebase";
+import Button from "@mui/material/Button";
+import {db,auth} from '../Firebase/firebase'
 import { UserAuth } from "../Firebase/AuthContext";
+import { useNavigate } from 'react-router-dom';
+import { collection ,addDoc} from "firebase/firestore";
+import TransitionModal from './Modal';
 
 function Navbar () {
+  const navigate = useNavigate()
 
     const {user, logOut} = UserAuth(auth);
+    const [feedback, setFeedback] = useState(false);
+
+
+    /********for adding Feedback ***********/
+  const [Response,setResponse]=useState("");
+  const feedbackRef=collection(db,"Feedback");
+  
+  const handleFeedback=async (e)=>{   
+    // console.log(user); 
+    e.preventDefault();
+    if(user===null)
+    {alert("Login to provide feedback");
+    return false;
+    }
+    else if(Response===""){
+      alert("Fill all the fields");
+      return false;
+    }else{
+    await addDoc(feedbackRef,{
+        Response,
+        author:{name:auth.currentUser.displayName,id:auth.currentUser.uid},
+        
+    }).then(()=>{alert("Feedback submitted")}).catch(err=>{alert(err.message)});
+
+    setResponse("");
+}
+};
+  /*************************/
+
     
     const handleSignOut = async()=>{
         try{
@@ -34,32 +70,34 @@ function Navbar () {
                         // (user.providerData[0].providerId === 'google.com')
                         // ? <li><a href="/my-activity">Dashboard</a></li>
                         // : <li><a href="/admin">Dashboard</a></li>
-                        //<li><a href="/my-activity">Dashboard</a></li>
                     }
                     
                     
-                    {/*<li>
-                    <div class="dropdown">
-                    <button class="dropbtn"><img src={Down_arrow}></img></button>
-                    <ul class="dropdown-content">
-                        <li>Publish <span></span><span></span><span></span><span></span></li>
-                        <li>Invite <span></span><span></span><span></span><span></span></li>
-                        <li>Feedback <span></span><span></span><span></span><span></span></li>
-                    </ul>
-                    </div>
-                    </li>*/}
                     
                 </ul>
                 
                 <div class="dropdown">
-                    <LoginIcon/>
+                <a class="login-icon"><i class="fa-regular fa-3x fa-circle-user"></i></a>
                     <ul class="dropdown-content">
-                        <li>Dashboard <span></span><span></span><span></span><span></span></li>
-                        <li>Feedback <span></span><span></span><span></span><span></span></li>
+                        <li onClick={()=> {navigate('/my-activity')}}>Dashboard </li>
+                        <li onClick={() => setFeedback(true)}>
+                        <TransitionModal 
+          title="Feedback" 
+          button={<span class="feedback-btn">Feedback </span>}
+          content="Your Feedback will help us to improve our website and your own experience. Kindly spare a minute to provide your valuable feeback. Thankyou.">             
+            <input type="text" onChange={(e)=>{setResponse(e.target.value)}}></input>
+            <Button  variant="contained"
+          align="center"
+          color="secondary"
+          sx={{ marginTop: "20px" ,width:"23em", backgroundColor:"#8152BD", marginLeft:"8em", height:"42px", fontFamily:"Montserrat", "&:hover": {backgroundColor: "#8152BD" } }}
+          onClick={handleFeedback}
+          > Submit</Button>
+          </TransitionModal>
+            </li>
 
                         {
-                    user? <li onClick={handleSignOut}>Logout <span></span><span></span><span></span><span></span></li> 
-                        : <li><a href="/loginpage">Login <span></span><span></span><span></span><span></span></a></li>
+                    user? <li onClick={handleSignOut}>Logout </li> 
+                        : <li onClick={()=> {navigate('/loginpage')}}>Login </li>
                         }
 
                         
